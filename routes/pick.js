e1d385719f24c10204b4cca7ca55c91a8f8229ec
Cody Garner <99354149+WinterGelom@users.noreply.github.com>
2025-04-02 17:14:13 -0500

    Made the Random picking of images working.

--- routes/pick.js
+++ routes/pick.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('randomitem', { title: 'A random item' });
 });
 
 module.exports = router;
